fb9cc5c82ddd545388d880be2bcd0ef8986942d7
Utkarsh Raj <raj.utkarsh001@gmail.com>
2024-06-23 14:10:26 +0530

    prod serve

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ CORS(app, resources={r"/*": {"origins": "*", "methods": "*", "allow_headers": "*
 
 if __name__ == '__main__':
     from waitress import serve
-    app.run(port=8080, debug=False)
+    serve(app, host="0.0.0.0", port=8080)
